de6913b0a27f02e9fb33f6511de37ec06c550035
TrilhaX <joaotrilha01@gmail.com>
2025-07-03 09:48:55 -0300

    Iniciado o projeto com react, tela da home esta praticamente feita, tela de login ja teve seu arquivo criado mas não foi iniciada e readMe feito tambem.

---
+++ metro+conectado/src/components/home.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const Home = () => {
+    const [menuAberto, setMenuAberto] = useState(false);
+
+    return (
+        <>
+            <div class="background-image"></div> {/* Não mexer nesse class*/}
+            <div
+                className={`botaoMenu ${menuAberto ? "moverBotao" : ""}`}
+                onClick={() => setMenuAberto(!menuAberto)}
+            >
+                ☰
+            </div>
+            <div className={`sidebar ${menuAberto ? "aberta" : ""}`}>
+                <ul>
+                    <hr />
+                    <li><a href="#">Início</a></li>
+                    <hr/>
+                    <li><a href="#">Sobre</a></li>
+                    <hr/>
+                    <li><a href="#">Contato</a></li>
+                    <hr/>
+                    <li><a href="#">Planos</a></li>
+                    <hr/>
+                    <li><a href="login">Login</a></li>
+                    <hr/>
+                    <li><a href="#">Sign Up</a></li> 
+                    <hr />
+                </ul>
+            </div>
+            <main>
+                <div className="containerHome">
+                    <h1>METRÔ + CONECTADO</h1>
+                    <div className="buttonsHome">
+                        <button id="mapasButton">Mapas</button>
+                        <button id="lotacaoButton">Lotação</button>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
+};
+
+export default Home;
