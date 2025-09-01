bee1b16f3dff6de5ceb343ff981db6ba33b58bcd
TrilhaX <joaotrilha01@gmail.com>
2025-09-01 11:35:13 -0300

    Tentando fazer funcionar no gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/home';
 import Login from './components/login';
 import SignUp from "./components/signup";
@@ -10,18 +10,18 @@ import Perfil from "./components/perfil"
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/metro-conectado">
       <Routes>
-        <Route path="/metro-conectado" element={<Home />} />
-        <Route path="/metro-conectado/login" element={<Login />} />
-        <Route path="/metro-conectado/perfil" element={<Perfil />} />
-        <Route path="/metro-conectado/signup" element={<SignUp />} />
-        <Route path="/metro-conectado/planos" element={<Planos />} />
-        <Route path="/metro-conectado/mapa" element={<Mapa />} />
-        <Route path="/metro-conectado/lotacao" element={<Lotacao />} />
-        <Route path="/metro-conectado/recoverPassword" element={<RecSenha />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/planos" element={<Planos />} />
+        <Route path="/mapa" element={<Mapa />} />
+        <Route path="/lotacao" element={<Lotacao />} />
+        <Route path="/recoverPassword" element={<RecSenha />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
