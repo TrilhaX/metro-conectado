f58ac94998bc913df60ed52414a337389b781915
Trilha <141366739+TrilhaX@users.noreply.github.com>
2025-10-03 09:30:13 -0300

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import "./home.css"; // Certifique-se de que este caminho está correto
+import "./home.css";
 
 const Home = () => {
     const [menuAberto, setMenuAberto] = useState(false);
