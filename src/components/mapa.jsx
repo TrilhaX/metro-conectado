4ac7c0b989431c75a8402cf5ea7e247ab918c67d
TrilhaX <joaotrilha01@gmail.com>
2025-11-03 08:29:09 -0300

    Iniciado o mapa.jsx e tambem iniciado o css, ainda haverá mudanças futuras para melhor estilização. Coloquei tambem em assets a image de fundo para representação visual do mapa no mapa.jsx

--- src/components/mapa.jsx
+++ src/components/mapa.jsx
@@ -1,9 +1,51 @@
-const Mapa = () => {
-    return(
-        <>
-        
-        </>
-    )
-}
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './mapa.css';
 
-export default Mapa
+export default function HomeScreen() {
+  const navigate = useNavigate();
+
+  const handlePress = (station) => {
+    navigate('/detail', { state: { station } });
+  };
+
+  return (
+    <div className="container">
+      <div
+        className="mapa"
+        style={{
+          backgroundImage:
+            "url('https://i0.wp.com/diariodostrilhos.com/wp-content/uploads/2024/01/mapa-transporte-2024-.jpeg?fit=1080%2C713&ssl=1')",
+        }}
+      >
+        <h2 className="titulo">
+          CLIQUE NA ESTAÇÃO DESEJADA PARA VER O STATUS DO METRÔ
+        </h2>
+
+        <button
+          className="botao"
+          style={{ top: '190px', left: '50px' }}
+          onClick={() => handlePress('Metrô Para Guarulhos')}
+        >
+          🚇 Metrô Para Guarulhos
+        </button>
+
+        <button
+          className="botao"
+          style={{ top: '250px', left: '30px' }}
+          onClick={() => handlePress('Metrô Para Congonhas')}
+        >
+          🚇 Metrô Para Congonhas
+        </button>
+
+        <button
+          className="botao"
+          style={{ top: '140px', right: '40px' }}
+          onClick={() => handlePress('Metrô Para Liberdade')}
+        >
+          🚇 Metrô Para Liberdade
+        </button>
+      </div>
+    </div>
+  );
+}
